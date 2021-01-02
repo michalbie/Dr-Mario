5fccb44767ef59408ffd313245f099f74b0acaac
Michal Biernacki <userofficeit@gmail.com>
2021-01-02 17:36:54 +0100

    Add clearing cells when they combo (at least 4 cells in row or column next to each other)

--- src/modules/BoardManagement/PillsManager.js
+++ src/modules/BoardManagement/PillsManager.js
@@ -5,7 +5,9 @@ import { Pill } from "./Pill.js";
 const PillsManager = class PillsManager {
     constructor() {
         this._cells = [];
+        this.currentPill = null;
         this.isBoostPressed = false;
+        this.oldPills = [];
     }
 
     set cells(newCells) {
@@ -13,6 +15,10 @@ const PillsManager = class PillsManager {
     }
 
     createNewPill = () => {
+        if (this.currentPill != null) {
+            this.oldPills.push(this.currentPill);
+            this.clearComboedCells();
+        }
         this.currentPill = new Pill(this._cells, this._cells[0][3], this._cells[0][4], this);
     };
 
@@ -57,6 +63,100 @@ const PillsManager = class PillsManager {
                 this.currentPill.currentFallingTime = this.currentPill.fallingTime;
             }
         });
+    };
+
+    clearComboedCells = () => {
+        const comboedCells = this.lookForCombos();
+
+        this.oldPills.forEach((oldPill) => {
+            if (comboedCells.includes(oldPill.pillCells.cell1)) {
+                oldPill.pillCells.cell1 = null;
+            } else if (comboedCells.includes(oldPill.pillCells.cell2)) {
+                oldPill.pillCells.cell2 = null;
+            }
+        });
+
+        comboedCells.forEach((comboCell) => {
+            comboCell.style.backgroundColor = "";
+        });
+    };
+
+    lookForCombos = () => {
+        let currentCombo = { color: "unknown", amount: 0, comboCells: [] };
+        let combosCells = [];
+
+        const scanByRows = () => {
+            for (let row = 0; row < 16; row++) {
+                for (let column = 0; column < 8; column++) {
+                    if (this._cells[row][column].style.backgroundColor == currentCombo.color) {
+                        currentCombo.amount += 1;
+                        currentCombo.comboCells.push(this._cells[row][column]);
+                    } else if (currentCombo.amount >= 4) {
+                        currentCombo.comboCells.forEach((comboCell) => {
+                            combosCells.push(comboCell);
+                        });
+                        currentCombo.comboCells = [this._cells[row][column]];
+                        currentCombo.color = this._cells[row][column].style.backgroundColor;
+                        currentCombo.amount = 1;
+                    } else if (this._cells[row][column].style.backgroundColor != "") {
+                        currentCombo.comboCells = [this._cells[row][column]];
+                        currentCombo.color = this._cells[row][column].style.backgroundColor;
+                        currentCombo.amount = 1;
+                    } else {
+                        currentCombo.comboCells = [];
+                        currentCombo.color = "unknown";
+                        currentCombo.amount = 0;
+                    }
+                }
+                if (currentCombo.amount >= 4) {
+                    currentCombo.comboCells.forEach((comboCell) => {
+                        combosCells.push(comboCell);
+                    });
+                }
+                currentCombo.comboCells = [];
+                currentCombo.color = "unknown";
+                currentCombo.amount = 0;
+            }
+        };
+
+        const scanByColumns = () => {
+            for (let column = 0; column < 8; column++) {
+                for (let row = 0; row < 16; row++) {
+                    if (this._cells[row][column].style.backgroundColor == currentCombo.color) {
+                        currentCombo.amount += 1;
+                        currentCombo.comboCells.push(this._cells[row][column]);
+                    } else if (currentCombo.amount >= 4) {
+                        currentCombo.comboCells.forEach((comboCell) => {
+                            combosCells.push(comboCell);
+                        });
+                        currentCombo.comboCells = [this._cells[row][column]];
+                        currentCombo.color = this._cells[row][column].style.backgroundColor;
+                        currentCombo.amount = 1;
+                    } else if (this._cells[row][column].style.backgroundColor != "") {
+                        currentCombo.comboCells = [this._cells[row][column]];
+                        currentCombo.color = this._cells[row][column].style.backgroundColor;
+                        currentCombo.amount = 1;
+                    } else {
+                        currentCombo.comboCells = [];
+                        currentCombo.color = "unknown";
+                        currentCombo.amount = 0;
+                    }
+                }
+                if (currentCombo.amount >= 4) {
+                    currentCombo.comboCells.forEach((comboCell) => {
+                        combosCells.push(comboCell);
+                    });
+                }
+                currentCombo.comboCells = [];
+                currentCombo.color = "unknown";
+                currentCombo.amount = 0;
+            }
+        };
+
+        scanByRows();
+        scanByColumns();
+
+        return combosCells;
     };
 };
 
